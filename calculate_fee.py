db5e5333fddba283a2a17a3604e700405f4f6daf
RokasT <rokas.tamulevicius28@gmail.com>
2019-12-19 18:29:30 +0200

    transaction input

--- calculate_fee.py
+++ calculate_fee.py
@@ -2,7 +2,12 @@ from bitcoin.rpc import RawProxy
 
 proxy = RawProxy()
 
-tx_id = '0627052b6f28912f2703066a912ea577f2ce4da4caa5a5fbd8a57286c345c2f2'
+tx_id = input("Enter transaction id: ")
+#Viena vertingiausių transakcijų Bitcoin tinkle
+#tx_id = '4410c8d14ff9f87ceeed1d65cb58e7c7b2422b2d7529afc675208ce2ce09ed7d'
+#0.06534852 BTC
+#tx_id = '0627052b6f28912f2703066a912ea577f2ce4da4caa5a5fbd8a57286c345c2f2'
+#0.00050000 BTC
 
 try:
     raw_tx = proxy.getrawtransaction(tx_id)
@@ -21,6 +26,8 @@ try:
         vout_sum += tx_out['value']
 
     transaction_fee = vin_sum - vout_sum
-    print(transaction_fee)
+
+    print("Transaction's id: " + tx_id)
+    print("Transaction's fee: " + str(transaction_fee) + " BTC")
 except KeyError:
     print(KeyError)
